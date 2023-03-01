00e47e30ca9381d3219aa171fae7a47d7e118317
AllanCheboiwo <kiplongeiallan@gmail.com>
2023-03-01 09:11:28 -0800

    Update validation.js

--- script/validation.js
+++ script/validation.js
@@ -1,16 +1,22 @@
 window.onload = function() {
     document.querySelector('#contactForm').addEventListener('submit', function(event) { 
-    // var email = document.querySelector('#email').value;
-    // if (ValidateEmail(email) == false) {
-    //     event.preventDefault();
-    //     document.querySelector('#email').style = 'border:solid 4px purple';
-    //     alert('Please fill in all required fields');
-    // }
     const required = document.querySelectorAll('.required');
+    const validateEmail = (email) => {
+        const re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+        return re.test(String(email).toLowerCase());
+      };
     for (let i = 0; i < required.length; i++) {
         required[i].addEventListener('change', function() {
             required[i].style = 'border-color:initial';
             });
+            if(required[i].id == "email"){
+                if(validateEmail(required[i].value) == false){
+                    event.preventDefault();
+                    required[i].style = 'border:solid 4px purple';
+                    alert("You have entered an invalid email address!")
+                    break;
+                }
+            }
        
             if (required[i].value == '') {
             event.preventDefault();
@@ -19,25 +25,12 @@ window.onload = function() {
             break;
             
         }
-        if(required[i].id == "email"){
-            if(ValidateEmail(required[i].value) == false){
-                event.preventDefault();
-                required[i].style = 'border:solid 4px purple';
-                alert('Please Enter a valid email address');
-                break;
-            }
-        }
     
     }
 });
-    function ValidateEmail(mail) 
-    {
-    if (/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(myForm.emailAddr.value))
-    {
-        return (true)
-    }
-        alert("You have entered an invalid email address!")
-        return (false)
-    }
+// function ValidateEmail(_mail) 
+// {
+
+// }
 
 }
